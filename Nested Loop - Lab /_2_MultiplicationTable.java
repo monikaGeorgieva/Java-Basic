83fdde97fdf0cbb779081ecab48d0b25b085eb3c
monikaGeorgieva <88885355+monikaGeorgieva@users.noreply.github.com>
2023-10-02 14:59:53 +0100

    Create _2_MultiplicationTable.java

---
+++ Nested Loop - Lab /_2_MultiplicationTable.java
@@ -0,0 +1,12 @@
+public class _2_MultiplicationTable {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10 ; i++) {
+            for (int j = 1; j <=10 ; j++) {
+                int total = i * j;
+                System.out.printf("%d * %d = %d\n",i,j,total);
+            }
+        }
+    }
+}
+//няма вход
+/
