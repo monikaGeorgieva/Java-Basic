f96c80f063f8700a3fddea573a3e9bae4fa5bf28
monikaGeorgieva <88885355+monikaGeorgieva@users.noreply.github.com>
2023-10-02 14:59:01 +0100

    Create _1_clock.java

---
+++ Nested Loop - Lab /_1_clock.java
@@ -0,0 +1,14 @@
+public class _1_clock {
+    public static void main(String[] args) {
+        for (int i = 0; i <=23 ; i++) {
+            for (int j = 0 ; j <= 59 ; j++) {
+
+                    System.out.printf("%d:%d\n",i,j);
+
+
+            }
+        }
+    }
+}
+//няма вход
+// 0:0 , 0:1, 0:2,0:3 .....
